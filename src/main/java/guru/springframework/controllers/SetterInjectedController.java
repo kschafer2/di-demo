24323baa0d258484d639ce5a3c9df01bb743efbd
Kyle Schafer <kschay@protonmail.com>
2019-02-27 13:32:07 -0600

    Add SetterGreetingService and ConstructorGreetingService Beans and injected both with respective injection controllers

--- src/main/java/guru/springframework/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -2,18 +2,20 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
 
-    @Autowired
     private GreetingService greetingService;
 
     public String sayHello(){
         return greetingService.sayGreeting();
     }
 
+    @Autowired
+    @Qualifier("setterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
