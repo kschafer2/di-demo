7222b3702272072d6253c91157fe0820a89c5b22
Kyle Schafer <kschay@protonmail.com>
2019-02-27 13:05:42 -0600

    Implement PropertyInjectedController as a Spring Bean for Spring auto dependency injection

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework;
 
 import guru.springframework.controllers.MyController;
+import guru.springframework.controllers.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,5 +15,7 @@ public class DiDemoApplication {
 		MyController controller = (MyController) ctx.getBean("myController");
 
 		controller.hello();
+
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 	}
 }
